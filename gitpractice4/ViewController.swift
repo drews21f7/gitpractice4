667ec5158e590edfc720ab7d9923f62f7384f20a
Drew Seeholzer <drews21f7@gmail.com>
2019-06-14 16:15:27 -0600

    Initial Commit

---
+++ gitpractice4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitpractice4
+//
+//  Created by Drew Seeholzer on 6/14/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
